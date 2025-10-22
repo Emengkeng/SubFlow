907c2f7f6c0c355fa8903bc3a77ef284f2f9e613
Emengkeng <blessedsonlove@gmail.com>
2025-10-22 14:33:07 +0100

    BREAKING CHANGE: completely generate new sql file

--- lib/db/migrations/0002_ambiguous_robin_chapel.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE "invitations" ADD COLUMN "token" text NOT NULL;--> statement-breakpoint
-ALTER TABLE "invitations" ADD COLUMN "created_at" timestamp DEFAULT now() NOT NULL;--> statement-breakpoint
-ALTER TABLE "teams" ADD COLUMN "organization_id" uuid NOT NULL;--> statement-breakpoint
-ALTER TABLE "teams" ADD CONSTRAINT "teams_organization_id_organizations_id_fk" FOREIGN KEY ("organization_id") REFERENCES "public"."organizations"("id") ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
-ALTER TABLE "invitations" ADD CONSTRAINT "invitations_token_unique" UNIQUE("token");
